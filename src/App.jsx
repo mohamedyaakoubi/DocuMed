5993aa2a914a79f273cf4342ccb4828d87aefe83
ahmedliouane0 <ahmedliouane0@gmail.com>
2024-08-23 12:59:52 +0100

    front

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Navbar } from './Pages/Navbar/Navbar';
-import { Home } from './Pages/Home/Home'
+import { Home } from './Pages/Home/Home';
 import { About } from './Pages/About/About';
 import {Contact} from './Pages/Contact/Contact';
 import { LogIn } from './Pages/LogIn/LogIn';
 import { SignUp } from './Pages/SignUp/SignUp';
 import { Footer } from './Pages/Footer/Footer';
 import {ClientDashboard} from './ClientComponents/ClientDashboard/ClientDashboard'
+import Navbar from './Pages/Navbar/Navbar'; 
+
 
 function App() {
   return (
@@ -19,7 +20,7 @@ function App() {
                       <Route path="/" element={<Home />} />
                       <Route path="/About" element={<About />} />
                       <Route path="/Contact" element={<Contact />} />
-                      <Route path="/Login" element={<LogIn />} />
+                      <Route path="/LogIn" element={<LogIn />} />
                       <Route path="/Signup" element={<SignUp />} />
                       <Route path="/ClientDashboard" element={<ClientDashboard />} />
                       <Route path="*" element={<Home />} />
