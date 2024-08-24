dcc4f4e6f05f6d85ebaf46725d1847a2a2de7142
mohamedyaakoubi <yakoubimohamed@fss.u-sfax.tn>
2024-08-24 09:56:44 +0100

    final

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,6 +1,8 @@
+///home.jsx
+
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import doc from '../../imgs/doctorrr.png';
+import doc from '../Navbar/doc.jpeg';
 import heart from '../Navbar/heart.png';
 import calander from '../Navbar/calander.jpeg';
 import helpdoc from '../Navbar/helpdoc.jpg';
@@ -9,75 +11,63 @@ import pat from './pat.jpg';
 
 import calen from './calen.png'
 import './Home.css'
-
+import './Home.css';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import { Button } from 'react-bootstrap';
 
 export const Home = () => {
 
-
-
-
   const navigate = useNavigate(); // Initialize the navigate function
-
 
   return (
     <div>
       
 
-      <div className="servicesSection1">
-        <h2>Who are we ?</h2>
-      </div>
-      <div className="contentContainer">
-      
-        <div className="statements">
-          
-          <p className="statement">enhancing both accessibility and convenience in healthcare. Patients can easily schedule appointments, access medical records, and consult with doctors from the comfort of their homes, reducing the need for in-person visits and long waiting times. For doctors, the app streamlines communication, enabling them to manage appointments, track patient progress, and provide timely advice or follow-ups, leading to more efficient and personalized care. Additionally, the app can support telemedicine services, expanding healthcare access to remote or underserved areas and improving overall patient outcomes.</p>
-          <br />
-          <p className="statement">An app that connects doctors with patients revolutionizes healthcare by fostering continuous and real-time communication. Patients can quickly reach out to their healthcare providers with concerns or questions, receiving prompt responses that can prevent minor issues from escalating. The app also supports better coordination of care, allowing multiple healthcare professionals to collaborate more effectively on a patient's treatment plan. Furthermore, with features like reminders for medication and appointments, patients are more likely to adhere to their prescribed treatments, enhancing the effectiveness of care. Overall, the app empowers patients with greater control over their health while enabling doctors to deliver more proactive and tailored care.</p>
-
-          
+      <div className="contentContainer" >
+        <div className="statements" style={{marginTop:"200px"}}>
+          <p className="statement , statement1" >Welcome at DocuMed</p>
+          <p className="statement , statement2">Connecting Doctors and Patients through the Digital word </p>
+          <p className="statement , statement3" >DocuMed is a palatform designed to facilitate the communication between the Doctor and the Patients . It offers a Digital Records , appointment taking and many others services that the will discover after your log in ! </p>
         </div>
 
         <div>
         <div className="imageContainer">
-          <img src={doc} alt="Document" className="docImage" />
+          <img src='/assets/doctori.png' alt="Document" className="docImage1"   style={{width:"500px",  }}/>
         </div>
       </div>
       </div>
+    <div className='all'>
       <div className="servicesSection">
-        <h2>Our Services for You</h2>
+          <h2>Our Services for You</h2>
+        </div>
+      <div className="services">
+        <div className="imageGroup">
+          <div className="imageItem">
+            <img src={heart} alt="Heart" className="heartImage" />
+            <p>Free access to all Doctors</p>
+          </div>
+          <div className="imageItem">
+            <img src={calen} alt="Calendar" className="calanderImage" />
+            <p>Pick the convinient appointment</p>
+          </div>
+          <div className="imageItem">
+            <img src={patient} alt="Patient" className="patientImage" />
+            <p>Online Docs availibility</p>
+          </div>
+        </div>
       </div>
-
-      <div className="imageGroup">
-        <div className="imageItem">
-          <img src={heart} alt="Heart" className="heartImage" />
-          <p>Care</p>
-        </div>
-        <div className="imageItem">
-          <img src={calen} alt="Calendar" className="calanderImage" />
-          <p>Appoitments</p>
-        </div>
-        <div className="imageItem">
-          <img src={patient} alt="Patient" className="patientImage" />
-          <p>secure filing</p>
-        </div>
-      </div>
-
+      </div>   
+    <div className='divii' >
       <div className="askdoc">
-        <h1>Are you a doctor?</h1>
-        <br />
-
+        <h1 className='alignili , areu'>Are you a doctor?</h1>
         <img src={helpdoc} alt="Help Document" className="docImage" />
-        <br />
-        <p>All communications, including appointment reminders, prescriptions, and follow-up instructions, are stored digitally. This helps in maintaining a comprehensive record of patient interactions and can be easily accessed when needed.</p>
+        <p className='alignili'>All communications, including appointment reminders, prescriptions, and follow-up instructions, are stored digitally. This helps in maintaining a comprehensive record of patient interactions and can be easily accessed when needed.</p>
       </div>
-
+    </div>
       <div className="askpatient">
         <h1>Are you a patient?</h1>
         <img src={pat} alt="pat" className="pat" />
-        <br />
         <p>Patients can actively participate in their healthcare by asking questions, tracking their symptoms, and receiving educational materials. This fosters a more collaborative approach to health management.</p>
       </div>
     </div>
